ec0304c441e187d68ed043ce32b3371aae5974df
MrDallOca <tdalloca@gmail.com>
2017-05-18 09:19:11 -0300

    Added new files to interface the DB

--- TDI_projeto_jstl/src/main/java/com/proj/tdi_projeto_jstl/DAO.java
+++ TDI_projeto_jstl/src/main/java/com/proj/tdi_projeto_jstl/DAO.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package maligno;
+package com.proj.tdi_projeto_jstl;
 
 import java.io.IOException;
 import java.sql.ResultSet;
